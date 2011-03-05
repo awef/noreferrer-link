90040e601dd4e3c74139bbec953838311f629db5
awef <idawef@gmail.com>
2011-03-05 18:59:01 +0900

    ミドルクリックがclickイベントを吐かなくなっていたため、mouseupも監視するよう変更

--- code/content_script.js
+++ code/content_script.js
@@ -1,17 +1,22 @@
 chrome.extension.sendRequest({type: 'GET_CONFIG'}, function(config) {
-  document.documentElement.addEventListener('click', function(e) {
-    if (e.target.nodeName === 'A') {
-      var a = e.target;
-
-      var rule = config.site[a.hostname] || config.global;
-      if (rule === 'ALL' || (rule === 'DIFF_DOMAIN' && location.host !== a.hostname)) {
-        if (!/(?:^|\s)noreferrer(?:$|\s)/.test(a.rel)) {
-          a.rel = (a.rel + ' noreferrer').trim();
+  var fn = function(node) {
+    if (node.nodeName === 'A') {
+      var rule = config.site[node.hostname] || config.global;
+      if (rule === 'ALL' || (rule === 'DIFF_DOMAIN' && location.host !== node.hostname)) {
+        if (!/(?:^|\s)noreferrer(?:$|\s)/.test(node.rel)) {
+          node.rel = (node.rel + ' noreferrer').trim();
         }
       }
     }
-    else if (e.target.parentNode) {
-      arguments.callee({target: e.target.parentNode});
+    else if (node.parentNode) {
+      fn(node.parentNode);
     }
+  };
+
+  document.documentElement.addEventListener('click', function(e) {
+    fn(e.target);
+  }, false);
+  document.documentElement.addEventListener('mouseup', function(e) {
+    fn(e.target);
   }, false);
 });
